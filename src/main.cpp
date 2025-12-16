3e47e33288bafe19d5c281da536e8ff26048e546
Globox <globox610@hotmail.com>
2025-12-16 14:54:41 +0100

    2025-12-16

--- src/main.cpp
+++ src/main.cpp
@@ -5,6 +5,7 @@ int main()
     auto window = sf::RenderWindow(sf::VideoMode({1920u, 1080u}), "CMake SFML Project");
     window.setFramerateLimit(144);
 
+
     while (window.isOpen())
     {
         while (const std::optional event = window.pollEvent())
